346cc79199769b9e0326ef398fc76927f08880c6
prince1998 <priyanshramnani21@gmail.com>
2021-06-10 01:53:43 +0530

    Implemented Maximum sum subarray of size K
    Implemented Maximum sum subarray of size K using Sliding Window Pattern

--- Sliding Window Pattern/3. Smallest Subarray with a given sum.py
+++ Sliding Window Pattern/3. Smallest Subarray with a given sum.py
@@ -1,18 +1,19 @@
 # Maximum sum subarray of size K - Sliding Window
 
-arr = [2, 1, 5, 2, 3, 2]
-sum_required = 7
+arr = [3, 4, 1, 1, 6]
+sum_required = 8
+min_len = 9999
 window_sum = 0
 window_start = 0
-min_len = 200000
 
 for window_end in range(0,len(arr)):
     window_sum += arr[window_end]
-    while  window_sum>=sum_required:
-        if min_len > ((window_end - window_start) + 1):
-            min_len = (window_end - window_start) + 1
+    while window_sum >= sum_required:
+        if min_len >= (window_end-window_start)+1:
+            min_len = (window_end-window_start)+1
         window_sum -= arr[window_start]
         window_start += 1
 
 print(min_len)
+
 
